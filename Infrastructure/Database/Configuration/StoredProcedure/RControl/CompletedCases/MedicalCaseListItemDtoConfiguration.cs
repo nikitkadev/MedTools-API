4b825f2885b708a72e41808761c4bb943ca5ffd7
nikitkadev <nekitosik.pav@gmail.com>
2026-08-18 17:15:51 +0700

    fix: исправить тип свойства в модели клинической группы, внести мелкие правки

--- Infrastructure/Database/Configuration/StoredProcedure/RControl/CompletedCases/MedicalCaseListItemDtoConfiguration.cs
+++ Infrastructure/Database/Configuration/StoredProcedure/RControl/CompletedCases/MedicalCaseListItemDtoConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 using Core.Dtos.RControl.CompletedCases;
 
 namespace Infrastructure.Database.Configuration.StoredProcedure.RControl.CompletedCases;
