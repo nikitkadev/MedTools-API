83719d3ca1cfc9ba8329a11f48285db4946b3f60
nikitkadev <nekitosik.pav@gmail.com>
2026-09-04 10:40:41 +0700

    feature: добавить сущность базы данных ContraindicationDbEntity и ее конфигурацию

---
+++ Infrastructure/Database/DbEntities/InvoiceStructure/ContraindicationDbEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Database.DbEntities.InvoiceStructure;
+
+public sealed class ContraindicationDbEntity
+{
+    public int Uid { get; set; }
+    public int OncologyCaseUid { get; set; }
+
+    public byte ContraindicationCode { get; set; }
+    public DateTime ContraindicationDate { get; set; }
+}
