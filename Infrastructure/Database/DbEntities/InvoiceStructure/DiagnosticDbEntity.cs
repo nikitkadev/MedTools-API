14a66e1fe66846aa0c94e3a6f8c016f76e6c66ee
nikitkadev <nekitosik.pav@gmail.com>
2026-09-04 10:01:37 +0700

    feature: добавить сущность базы данных DiagnosticDbEntity и ее конфигурацию

---
+++ Infrastructure/Database/DbEntities/InvoiceStructure/DiagnosticDbEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Database.DbEntities.InvoiceStructure;
+
+public sealed class DiagnosticDbEntity
+{
+    public int Uid { get; set; }
+    public int OncologyCaseUid { get; set; }
+
+    public byte? DiagnosticType { get; set; }
+    public int? DiagnosticCode { get; set; }
+    public int? DiagnosticResultCode { get; set; }
+    public DateTime? SpecimenCollectionDate { get; set; } 
+    public byte? IsResultReceived { get; set; }
+
+}
